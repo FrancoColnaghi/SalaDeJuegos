20cf2bcca71f0360798217b235fa46099e77fab2
Franco Colnaghi <colnaghi.fr@gmail.com>
2024-06-25 12:01:36 -0300

    correccion error vercel

--- src/components/alinea4/Alinea4.jsx
+++ src/components/alinea4/Alinea4.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../../app.css";
+import "../../App.css";
 import { Celda } from "./Celda";
 import { controlarGanador } from "./ganador.js";
 import { Header } from "../Header.jsx";
